dfef681ebd664ae8e0010e2313bb90f4454fc369
Karen Gomes <kngs@ic.ufal.br>
2018-12-11 00:55:43 -0300

    feature(test):testing junit 5

---
+++ src/test/java/com/karenngomes/sistema/TestStringBuilder.java
@@ -0,0 +1,18 @@
+package com.karenngomes.sistema;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+import com.karenngomes.sistema.StringBuilder;
+
+public class TestStringBuilder {
+	
+	@Test
+	void testJuntaTexto() {
+		 StringBuilder str = new StringBuilder();
+		 String result = str.juntaText("texto", "junto");
+		 
+		 assertEquals("texto junto", result, "nao foi");
+	}
+	
+}
