e647de209d9e98d815e70b5a46845c3b48428474
Karen Gomes <kngs@ic.ufal.br>
2019-02-08 03:43:45 -0300

    feat(test): verifica se está mudando certo o nome da disciplina
    #6

--- src/test/java/com/karenngomes/sistema/model/SubjectTest.java
+++ src/test/java/com/karenngomes/sistema/model/SubjectTest.java
@@ -1,5 +1,28 @@
 package com.karenngomes.sistema.model;
 
-public class SubjectTest {
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.not;
 
+
+import org.junit.jupiter.api.Test;
+
+import com.karenngomes.sistema.utils.AcademicTypes;
+
+public class SubjectTest {
+	
+	private static Course course = new Course("Computer Science", AcademicTypes.UNDERGRADUATE);
+	
+	@Test
+	public void testSetSubjectName() {
+	
+		Subject subject = new Subject("Software testing", "1234", AcademicTypes.UNDERGRADUATE, course);
+		String oldSubjectName = subject.getName();
+		
+		subject.setName("Data structures");
+		
+		assertThat(oldSubjectName, not(equalTo(subject.getName())));
+		
+	}
+	
 }
