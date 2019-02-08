c12422d876a4ceba59fc1bf65d1eae09e60d1c9b
Karen Gomes <kngs@ic.ufal.br>
2019-02-08 03:33:00 -0300

    feat(Class): Validação com mensagem do tamanho da variável name
    #1

--- src/main/java/com/karenngomes/sistema/model/University.java
+++ src/main/java/com/karenngomes/sistema/model/University.java
@@ -1,6 +1,7 @@
 package com.karenngomes.sistema.model;
 
-import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -8,7 +9,11 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.Length.List;
+
+// import org.hibernate.validator.constraints.Length;
 
 // import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -21,22 +26,28 @@ public class University {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
-	
+
 	@NotNull
-	@Size(min=4, max=50)
+
+	@List({ @Length(min = 4, message = "The field must be at least 4 characters"),
+			@Length(max = 20, message = "The field must be less than 20 characters") })
 	private String name;
-	
+
 	@OneToMany
-	ArrayList <Department> departments = new ArrayList<Department>();
-	
+	private Set<Department> departments = new HashSet<Department>();
+
 	public University(String name) {
 		this.name = name;
 	}
-	
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
 	public void setDepartment(Department department) {
 		this.departments.add(department);
 	}
-	
+
 	// add Equals and hash code
-	
+
 }
