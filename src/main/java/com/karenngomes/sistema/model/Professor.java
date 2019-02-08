e2140bf05da6fa54cc06fca85e3befd7eebc46bc
Karen Gomes <kngs@ic.ufal.br>
2019-02-08 03:42:39 -0300

    feat(class): Adição de métodos set e variável siap
    #3

--- src/main/java/com/karenngomes/sistema/model/Professor.java
+++ src/main/java/com/karenngomes/sistema/model/Professor.java
@@ -1,11 +1,14 @@
 package com.karenngomes.sistema.model;
 
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 // import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -18,19 +21,30 @@ public class Professor {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
-	
-	private String name;
-	private ArrayList <Subject> subjects = new ArrayList<Subject>();
 
-	
-	public Professor(String name) {
+	private String name;
+	private int siap;
+
+	@OneToMany
+	private Set<Subject> subjects = new HashSet<Subject>();
+
+	public Professor(String name, int siap) {
+		this.name = name;
+		this.siap = siap;
+	}
+
+	public void setName(String name) {
 		this.name = name;
 	}
-	
+
+	public void setSiap(int siap) {
+		this.siap = siap;
+	}
+
 	public void setSubject(Subject subject) {
 		this.subjects.add(subject);
 	}
-	
+
 	// add Equals and hash code
-	
+
 }
